d935e716461d7d615e6191311655fb63a141d185
thanh711 <thanhsonhoc00@gmail.com>
2022-09-16 17:47:29 +0700

    node

---
+++ src/components/footer.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+export class Footer extends Component {
+  render() {
+    return (
+      <div className="footer">
+        <footer className="py-5 bg-dark">
+          <div className="container">
+            <p className="m-0 text-center text-white">
+              Copyright © Your Website 2022
+            </p>
+          </div>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default Footer;
